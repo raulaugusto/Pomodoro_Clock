882e902bd9ca1dcc41f0853c4db9d8ebf33e4e57
Raul Santos <contatoraulaugusto@gmail.com>
2025-12-08 20:11:56 -0300

    Feat: Capture value from minutes inputs and store in variables, also updates the timer value

--- index.js
+++ index.js
@@ -4,3 +4,29 @@ const restTimeInput = document.getElementById("rest");
 const startButton = document.getElementById("startTimer");
 const resetButton = document.getElementById("resetTimer");
 const alternateButton = document.getElementById("alternate");
+
+let focusTimerValue = 25;
+let restTimerValue = 5;
+let timeLeft = focusTimerValue;
+
+focusTimeInput.addEventListener("focusout", () => {
+  const timerValue = focusTimeInput.value;
+  if (timerValue > 60) {
+    alert("O valor máximo é de 60 minutos");
+    focusTimeInput.value = 60;
+    focusTimerValue = 60;
+  }
+  timer.textContent = `${timerValue}:00`;
+  timeLeft = timerValue;
+});
+
+restTimeInput.addEventListener("focusout", () => {
+  const timerValue = restTimeInput.value;
+  if (timerValue > 60) {
+    alert("O valor máximo é de 60 minutos");
+    restTimeInput.value = 60;
+    restTimerValue = 60;
+  } else {
+    restTimerValue = timerValue;
+  }
+});
