99188891e2195e7429f3ed25cb6440a507a636e1
Raul Santos <contatoraulaugusto@gmail.com>
2025-12-08 19:29:15 -0300

    Selected all the interactive elements from the DOM

---
+++ index.js
@@ -0,0 +1,6 @@
+const timer = document.getElementById("timer");
+const focusTimeInput = document.getElementById("focus");
+const restTimeInput = document.getElementById("rest");
+const startButton = document.getElementById("startTimer");
+const resetButton = document.getElementById("resetTimer");
+const alternateButton = document.getElementById("alternate");
